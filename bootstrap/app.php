3cd4624b5fe73c997508fccf57e0be44b1c930f9
Jose Juan Lepe <joselepeperez@gmail.com>
2026-05-27 15:04:28 +0200

    fix: forzar https y confiara en proxy de railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
+        $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
+        
 
         $middleware->alias([
             'tutor' => \App\Http\Middleware\EnsureTutor::class,
