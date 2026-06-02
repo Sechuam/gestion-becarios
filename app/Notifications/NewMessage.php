b0e0102578b6ee5582d9e485b9160598c6ae7da2
Jose Juan Lepe <joselepeperez@gmail.com>
2026-06-02 12:51:31 +0200

    Store message notifications synchronously

--- app/Notifications/NewMessage.php
+++ app/Notifications/NewMessage.php
@@ -4,15 +4,10 @@ namespace App\Notifications;
 
 use App\Models\Message;
 use App\Models\MessageConversation;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class NewMessage extends Notification implements ShouldQueue
+class NewMessage extends Notification
 {
-    use Queueable;
-
     public function __construct(
         public Message $message,
         public MessageConversation $conversation,
